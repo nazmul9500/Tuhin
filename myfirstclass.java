18bcffe535c7462f99c9b2247cd1100f4d6e752a
nazmul9500 <nazmul9500@yahoo.com>
2019-09-27 21:02:22 -0400

    Day_01

---
+++ myfirstclass.java
@@ -0,0 +1,34 @@
+package Day_1;
+
+public class myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+System.out.println("This is my First Java program");
+
+System.out.println("");
+System.out.println("");
+System.out.println(" **" + "      *" + "*");
+System.out.println(" *  *" + "   * *");
+System.out.println(" *   *" + " *  *");
+System.out.println(" *    *" + "   *");
+
+
+System.out.println("");
+System.out.println("");
+System.out.println( "    "+" **" + "    ");
+System.out.println( "    "+" *****" + "      " + "");
+System.out.println( "    "+" *******" + "    ");
+System.out.println( "    "+" ********" + "      " + "");
+System.out.println( "    "+" *********" + "    ");
+System.out.println( "    "+" ********" + "   ");
+System.out.println( "    "+" *******" + "      " + "");
+System.out.println( "    "+" *****" + "    ");
+System.out.println( "    "+" **" + "    ");
+}
+
+}
+	
+	
